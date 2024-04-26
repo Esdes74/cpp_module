94b9c677cc52860a309d887942a030bbb6533148
eslamber <eslamber@student.42.fr>
2024-04-26 19:06:44 +0200

    save

--- cp5/ex00/Bureaucrat.cpp
+++ cp5/ex00/Bureaucrat.cpp
@@ -6,8 +6,79 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:05:13 by eslamber          #+#    #+#             */
-/*   Updated: 2024/04/26 14:05:26 by eslamber         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:48:01 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+// Constructeurs
+Bureaucrat::Bureaucrat()
+{
+	name = "";
+	grade = 149;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{
+	this->name = name;
+	this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &cpy)
+{
+	if (this != &cpy)
+		*this = cpy;
+}
+
+// Surcharge d'opérateur
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &cpy)
+{
+	this->name = cpy.name;
+	this->grade = cpy.grade;
+}
+
+// Getters
+const std::string	&Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+const int			&Bureaucrat::getGrade() const
+{
+	return (this->grade);
+}
+
+// Fonctions membres
+void				Bureaucrat::incrementGrade()
+{
+	if (this->grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (this->grade > 149)
+		throw Bureaucrat::GradeTooLowException();
+	this->grade--;
+}
+
+void				Bureaucrat::decrementGrade();
+
+// Exceptions
+const char	*Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
+
+// Destructeur
+Bureaucrat::~Bureaucrat()
+{
+	return ;
+}
+
+std::ostream	&operator<<(std::ostream os, const Bureaucrat &out)
+{
+	os << out.getName() << "is graded : " << out.getGrade() << std::endl;
+}
--- cp5/ex00/Bureaucrat.hpp
+++ cp5/ex00/Bureaucrat.hpp
@@ -6,13 +6,56 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:05:05 by eslamber          #+#    #+#             */
-/*   Updated: 2024/04/26 14:06:05 by eslamber         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:42:11 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <stdexcept>
+# include <string>
 
+class Bureaucrat
+{
+	private:
+		std::string	name;
+		int			grade;
+	
+	public:
+		// Constructeurs
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &cpy);
+
+		// Surcharge d'opérateur
+		Bureaucrat			&operator=(const Bureaucrat &cpy);
+
+		// Getters
+		const std::string	&getName() const;
+		const int			&getGrade() const;
+
+		// Fonctions membres
+		void				incrementGrade();
+		void				decrementGrade();
+
+		// Classes d'exceptions
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				virtual const char	*what() const noexcept override;
+		}
+		
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				virtual const char	*what() const noexcept override;
+		}
+
+		// Destructeur
+		~Bureaucrat();
+}
+
+std::ostream	&operator<<(std::ostream os, const Bureaucrat &out);
 
 #endif
