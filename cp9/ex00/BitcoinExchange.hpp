fb973750011b4a280a36e2ec158678505de2e4a9
eslamber <eslamber@student.42.fr>
2024-05-14 18:02:27 +0200

    Save

--- cp9/ex00/BitcoinExchange.hpp
+++ cp9/ex00/BitcoinExchange.hpp
@@ -6,30 +6,34 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 11:57:54 by eslamber          #+#    #+#             */
-/*   Updated: 2024/05/06 12:41:51 by eslamber         ###   ########.fr       */
+/*   Updated: 2024/05/06 18:53:26 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
-# include BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
 # include <map>
+# include <string>
 
 class BitcoinExchange
 {
 	private:
-		std::map<std::string, float>	_change;
+		static std::map<std::string, float>	*_change;
 
 		// Constructeur
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange &cpy);
 
 		// Surcharge d'operateur
-		BitcoinExchange			&operator=(const BitcoinExchange &cpy);
+		BitcoinExchange					&operator=(const BitcoinExchange &cpy);
+
+		// Getter
+		static std::map<std::string, float>	&getChange();
 
 	public:
 		// Fonctions membres
-		static BitcoinExchange	&getInstance();
+		static const BitcoinExchange	*getInstance();
 
 		// Destructeur
 		~BitcoinExchange();
