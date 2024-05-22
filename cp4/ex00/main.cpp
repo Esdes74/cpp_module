a6c3cc0a3d4fd9c641649748ee5be903e7e93d9b
eslamber <estelambert74@gmail.com>
2024-05-22 13:44:42 +0200

    ajout de tests pour cp4/ex00

--- cp4/ex00/main.cpp
+++ cp4/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 16:47:34 by eslamber          #+#    #+#             */
-/*   Updated: 2024/04/29 16:30:48 by eslamber         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:42:52 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,42 @@
 
 int	main()
 {
+	std::cout << "============== Right Tests ===============" << std::endl;
 	const Animal*	ani = new Animal();
 	const Animal*	dog = new Dog();
-	Animal*	cat = new Cat();
-	Animal	cat1("cat1");
+	Animal*			cat = new Cat();
+	Animal			cat1("cat1");
 
-	std::cout << dog->getType() << " " << std::endl;
-	std::cout << cat->getType() << " " << std::endl;
+	std::cout << cat->getType() << " : ";
 	cat->makeSound(); //will output the cat sound!
+	std::cout << std::endl;
+	std::cout << dog->getType() << " : ";
 	dog->makeSound();
+	std::cout << std::endl;
+	std::cout << ani->getType() << " : ";
 	ani->makeSound();
+	std::cout << std::endl;
 	cat1 = *ani;
-	std::cout << cat1.getType() << " " << std::endl;
+	std::cout << cat1.getType() << std::endl;
 	delete ani;
 	delete dog;
 	delete cat;
+
+
+	std::cout << "\n============== Wrong Tests ===============" << std::endl;
+	const WrongAnimal*	ani2 = new WrongAnimal();
+	WrongAnimal*	cat2 = new WrongCat();
+	WrongAnimal		cat3;
+
+	std::cout << cat2->getType() << " : ";
+	cat2->makeSound();
+	std::cout << std::endl;
+	std::cout << ani2->getType() << " : ";
+	ani2->makeSound();
+	std::cout << std::endl;
+	cat3 = *ani2;
+	std::cout << cat3.getType() << std::endl;
+	delete ani2;
+	delete cat2;
 	return 0;
 }
